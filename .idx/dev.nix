683ea375190b34599603c26b0b77d2513b5c93e2
Kenneth White <lapster44@gmail.com>
2026-01-09 14:28:54 +0000

    Model Change

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,6 +10,10 @@
   ];
   # Sets environment variables in the workspace
   env = {};
+  
+  # Enables Docker in the workspace
+  services.docker.enable = true;
+
   # This adds a file watcher to startup the firebase emulators. The emulators will only start if
   # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
